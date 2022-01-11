49ec3022b8e738f306e1b300c46e6d7a5af19313
Sergey-Yurtaev <engineerk2@mail.ru>
2022-01-11 18:32:00 +0300

    Initial Commit

---
+++ CoreDataTaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTaskApp
+//
+//  Created by Sergey Yurtaev on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
